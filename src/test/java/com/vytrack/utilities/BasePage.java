778f81441b8169ab20d1f42165a6a63b86ab8cfc
Ali Sariboga <alican.sariboga.acs@gmail.com>
2023-07-02 14:28:50 +0100

    added apache poi and xlsx file

--- src/test/java/com/vytrack/utilities/BasePage.java
+++ src/test/java/com/vytrack/utilities/BasePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+public abstract class BasePage {
     //we don't want to access these variables outside
     private static final Logger logger = LogManager.getLogger();
 
